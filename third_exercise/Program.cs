e51c3d18c7c99b3a596a28e81fbbee30610c1e57
Zakhar <seriy.zachar@yandex.by>
2022-08-25 19:35:38 +0300

    exercise 3

--- third_exercise/Program.cs
+++ third_exercise/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿int first, second;
+Console.Write("Введите первое число: ");
+first = Console.Read();
+Console.Write("Введите второе число: ");
+second = Console.Read();
+Console.WriteLine(first+second);
